e1bbbc375265597ea08cc3d7d973df502d7fd18f
ongmin <bigbakingoven@gmail.com>
2017-08-31 01:08:10 +0800

    first commit

---
+++ index.ios.js
@@ -0,0 +1,49 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} from 'react-native';
+
+import SearchPage from './SearchPage';
+
+class PropertyFinder extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+}
+});
+
+AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
